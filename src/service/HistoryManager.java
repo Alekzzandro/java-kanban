cba8c931af3d47c43d7f9aa5abf9bfd2f09229df
Torchinskij Alexandr <alekzz02@mail.ru>
2024-12-05 18:02:30 +0500

    Fix all again!

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -4,8 +4,12 @@ import model.Task;
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
+
     void clear();
 }
--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -1,32 +1,58 @@
 package service;
+
 import model.Epic;
 import model.SubTask;
 import model.Task;
+
 import java.util.List;
 
 public interface TaskManager {
+
     Task createTask(Task task);
+
     Epic createEpic(Epic epic);
+
     SubTask createSubTask(SubTask subTask);
+
     boolean updateTask(Task task);
+
     boolean updateEpic(Epic epic);
+
     boolean updateSubTask(SubTask subTask);
+
     Task getTaskById(int taskId);
+
     Epic getEpicById(int epicId);
+
     SubTask getSubTaskById(int subTaskId);
+
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<SubTask> getSubTasks();
+
     List<SubTask> getSubTasksByEpic(int epicId);
+
     void deleteTask(int taskId);
+
     void deleteEpic(int epicId);
+
     void deleteSubTask(int subTaskId);
+
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubTasks();
+
     void addTask(Task task);
+
     void removeTask(int id);
+
     Task getTask(int id);
+
     List<Task> getAllTasks();
+
     List<Task> getHistory();
 }
