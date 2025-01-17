2010a2ed119ce085303c6b4981a26c67315f4529
Torchinskij Alexandr <alekzz02@mail.ru>
2025-01-18 03:02:48 +0500

    New branch with completed tasks!

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,22 +1,11 @@
 package service;
 
-import model.Task;
-import model.Epic;
-import model.SubTask;
-import model.TaskTypes;
 import exception.ManagerLoadFileException;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
 public class Managers {
-    public static HistoryManager getDefaultHistory() {
-        return new InMemoryHistoryManager();
-    }
-
-    public static TaskManager getDefaultTaskManager() {
-        return new InMemoryTaskManager();
-    }
 
     public static TaskManager getFileBackedTaskManager(Path path) throws IOException, ManagerLoadFileException {
         return new FileBackedTaskManager(path);
