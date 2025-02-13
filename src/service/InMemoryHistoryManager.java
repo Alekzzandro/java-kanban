5acc1627f3e51893a978f66c492a75f4b731afb1
Torchinskij Alexandr <alekzz02@mail.ru>
2025-02-14 03:44:07 +0500

    Try to fix all remarks!

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -1,28 +1,35 @@
 package service;
+
 import model.Task;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 public class InMemoryHistoryManager implements HistoryManager {
     private final Map<Integer, Node<Task>> nodeMap = new HashMap<>();
     private Node<Task> head;
     private Node<Task> tail;
+
     private static class Node<T> {
         T item;
         Node<T> next;
         Node<T> prev;
+
         Node(Node<T> prev, T element, Node<T> next) {
             this.item = element;
             this.next = next;
             this.prev = prev;
         }
     }
+
     @Override
     public void add(Task task) {
         remove(task.getId());
         linkLast(task);
     }
+
     @Override
     public void remove(int id) {
         Node<Task> node = nodeMap.remove(id);
@@ -30,6 +37,7 @@ public class InMemoryHistoryManager implements HistoryManager {
             removeNode(node);
         }
     }
+
     @Override
     public List<Task> getHistory() {
         List<Task> history = new ArrayList<>();
@@ -40,6 +48,7 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
         return history;
     }
+
     private void linkLast(Task task) {
         final Node<Task> oldTail = tail;
         final Node<Task> newNode = new Node<>(oldTail, task, null);
@@ -51,6 +60,7 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
         nodeMap.put(task.getId(), newNode);
     }
+
     private void removeNode(Node<Task> node) {
         final Node<Task> next = node.next;
         final Node<Task> prev = node.prev;
